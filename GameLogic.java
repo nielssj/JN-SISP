95793668080bbc9ffa2d958222925cc29c894698
Niels Soeholm <nm@9la.dk>
2013-03-07 17:46:26 +0100

    Fixed issues with GameFinished and implemented basic minimax.

--- GameLogic.java
+++ GameLogic.java
@@ -1,13 +1,9 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 
 public class GameLogic implements IGameLogic {
     private int n = 0;
     private int m = 0;
     private int playerID;
     private GameState state;
-    private Queue<int[]> newMoves;
     
     public GameLogic() {
         //TODO Write your implementation for this method
@@ -18,55 +14,133 @@ public class GameLogic implements IGameLogic {
         this.m = m;
         this.playerID = playerID;
         this.state = new GameState(n, m);
-        this.newMoves = new LinkedList<int[]>();
     }
 	
-    public Winner gameFinished() {
-        // Is it a tie?
+    public Winner gameFinished() 
+    {
+        return stateGameFinished(this.state);
+    }
+    
+    private Winner stateGameFinished(GameState state)
+    {
+    	// Did anyone win? (3)
+    	int[] move = state.getLastMove();
+		int x = move[0];
+		int y = move[1];
+		int p = state.ps(x, y);
+		if (p > 0) {
+			if (exploreField(x, y, 4, state)) return GameState.winnerFromInt(p);
+		}
+		
+		// Is it a tie?
     	boolean tie = true;
     	for(int t = 0; t < n; t++) {
     		tie = tie && !(state.coinsInColumn(t) < m);
     	}
     	if(tie) return Winner.TIE;
-
-    	// Did anyone win?
-    	while(!newMoves.isEmpty()) {
-    		int[] move = newMoves.poll();
-    		int x = move[0];
-			int y = move[1];
-    		int p = state.ps(x, y);
-    		if (p > 0) {
-				if ((p == state.ps(x, y+1) && p == state.ps(x, y+2) && p == state.ps(x, y+3))
-    				|| (p == state.ps(x+1, y) && p == state.ps(x+2, y) && p == state.ps(x+3, y))
-    				|| (p == state.ps(x-1, y) && p == state.ps(x-2, y) && p == state.ps(x-3, y))
-					|| (p == state.ps(x, y+1) && p == state.ps(x, y+2) && p == state.ps(x, y+3))
-    				|| (p == state.ps(x, y-1) && p == state.ps(x, y-2) && p == state.ps(x, y-3))
-    				|| (p == state.ps(x+1, y+1) && p == state.ps(x+2, y+2) && p == state.ps(x+3, y+3))
-    				|| (p == state.ps(x+1, y-1) && p == state.ps(x+2, y-2) && p == state.ps(x+3, y-3))
-					|| (p == state.ps(x-1, y+1) && p == state.ps(x-2, y+2) && p == state.ps(x-3, y+3))
-					|| (p == state.ps(x-1, y-1) && p == state.ps(x-2, y-2) && p == state.ps(x-3, y-3)))
-				{
-					return GameState.winnerFromInt(p);
-				}
-			}
-    	}
     	
         return Winner.NOT_FINISHED;
     }
+    
+    private boolean exploreField(int x, int y, int max, GameState state) 
+    {    	
+    	if (1 + explore(x, y, -1, -1, 0, state) + explore(x, y, 1, 1, 0, state) >= max ||
+    		1 + explore(x, y, 0, -1, 0, state) >= max ||
+    		1 + explore(x, y, -1, 0, 0, state) + explore (x, y, 1, 0, 0, state) >= max ||
+    		1 + explore(x, y, -1, 1, 0, state) + explore(x, y, 1, -1, 0, state) >= max) return true;
+    	
+    	return false;
+    }
+    
+    private int explore(int x, int y, int dx, int dy, int level, GameState state)
+    {
+    	if (state.ps(x + dx, y + dy) == GameState.opponent(state.getCurrentPlayer())) return explore(x+dx, y+dy, dx, dy, level+1, state);
+    	else return level;
+    }
 
     public void insertCoin(int column, int playerID) {
-    	int[] move = state.madeMove(playerID, column);
-    	newMoves.add(move);
+    	state.madeMove(column);
     }
 
     public int decideNextMove() {
         // Put coin in left-most column, that isn't full already.
-    	for(int i = 0; i < n; i++) {
+    	/*for(int i = 0; i < n; i++) {
     		if(state.coinsInColumn(i) < m) {
     			return i;
     		}
     	}
-    	
-        return 0;
+    	return -1;*/
+    	return minmaxDecision(this.state);
     }
+    
+    private int minmaxDecision(GameState state)
+    {
+    	// Loop through potential actions (columns)
+    	int resultUtil = Integer.MIN_VALUE;
+    	int resultAction = -1;
+    	for(int i = 0; i < n; i++)
+    	{
+    		if(state.coinsInColumn(i) < m)
+    		{
+    			int val = minValue(state.result(i));
+    			System.out.println("Column " + i + " has utility " + val);
+    			if(resultUtil < val)
+				{
+    				resultAction = i;
+    				resultUtil = val;
+				}
+    		}
+    	}
+    	return resultAction;
+    }
+    
+    private int minValue(GameState state)
+    {
+    	Winner winner = stateGameFinished(state);
+    	if(winner != Winner.NOT_FINISHED)
+    	{
+    		return utility(winner);
+    	}
+    	
+    	int result = Integer.MAX_VALUE;
+    	for(int i = 0; i < n; i++)
+    	{
+    		if(state.coinsInColumn(i) < m)
+    		{
+	    		int val = maxValue(state.result(i));
+				if(result > val) result = val;
+    		}
+    	}
+    	
+    	return result;
+    }
+    
+    private int maxValue(GameState state)
+    {
+    	Winner winner = stateGameFinished(state);
+    	if(winner != Winner.NOT_FINISHED)
+    	{
+    		return utility(winner);
+    	}
+    	
+    	int result = Integer.MIN_VALUE;
+    	for(int i = 0; i < n; i++)
+    	{
+    		if(state.coinsInColumn(i) < m)
+    		{
+        		int val = minValue(state.result(i));
+    			if(result < val) result = val;
+    		}
+    	}
+    	
+    	return result;
+    }
+    
+    private int utility(Winner winner)
+    {
+    	if(winner == Winner.PLAYER1 && this.playerID == 1) return 1;
+    	else if(winner == Winner.PLAYER2 && this.playerID == 2) return 1;
+    	return -1;
+    }
+    
 }
--- GameState.java
+++ GameState.java
@@ -2,26 +2,54 @@
 public class GameState {
 	private int n;
 	private int m;
+	private int currentPlayer;
 	private int[] cic;
 	private int[][] board;
+	private int[] lastMove;
 	
 	
 	public GameState(int n, int m) {
 		this.n = n;
 		this.m = m;
+		this.currentPlayer = 1;
 		this.cic = new int[n];
 		this.board = new int[n][m];
+		this.lastMove = new int[] {-1,-1};
 	}
 	
-	public int[] madeMove(int player, int c) {
+	public GameState(int n, int m, int currentPlayer, int[] cic, int[][] board, int[] lastMove)
+	{
+		this.n = n;
+		this.m = m;
+		this.cic = cic;
+		this.board = board;
+		this.currentPlayer = currentPlayer;
+		this.lastMove = lastMove;
+	}
+	
+	public void madeMove(int c) {
 		int r = cic[c]++;
-		board[c][r] = player;
-		return new int[] {c,r};
+		board[c][r] = currentPlayer;
+		
+		if (currentPlayer == 1) currentPlayer = 2;
+		else currentPlayer = 1;
+		
+		lastMove = new int[] {c,r};
 	}
 	
 	public int coinsInColumn(int column)
 	{
 		return cic[column];
+	}
+	
+	public int getCurrentPlayer()
+	{
+		return currentPlayer;
+	}
+	
+	public int[] getLastMove()
+	{
+		return lastMove;
 	}
 	
 	public int ps(int x, int y)
@@ -30,9 +58,28 @@ public class GameState {
 		return board[x][y];
 	}
 	
+	public GameState result(int column)
+	{
+		int[][] newboard = new int[n][1];
+		for(int i=0; i < n; i++)
+		{
+			newboard[i] = this.board[i].clone();
+		}
+		
+		GameState newState = new GameState(this.n, this.m, this.currentPlayer, this.cic.clone(), newboard, this.lastMove.clone());
+		newState.madeMove(column);
+		return newState;
+	}
+	
 	public static IGameLogic.Winner winnerFromInt(int playerID)
 	{
 		if (playerID == 1) return IGameLogic.Winner.PLAYER1;
 		else return IGameLogic.Winner.PLAYER2;
+	}
+	
+	public static int opponent(int playerID) 
+	{
+		if (playerID == 1) return 2;
+		else return 1;
 	}
 }
